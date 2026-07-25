04284f44af5ba01d4ece355e8db39711262ef7a7
MyNameIs3rik <erikkocurek@gmail.com>
2026-07-25 18:33:33 +0200

    small changes

--- scripts/enemies/fire.gd
+++ scripts/enemies/fire.gd
@@ -3,12 +3,15 @@ extends Area2D
 @export var max_health: int = 5
 var health: int = max_health
 
+var fire_place: Area2D
+
 func _ready():
 	$AnimatedSprite2D.play()
 
 func take_damage(damage: int) -> void:
 	health -= damage
 	if health <= 0:
+		fire_place.decrement()
 		queue_free()
 	display_damage()
 
--- scripts/world/pole.gd
+++ scripts/world/pole.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+var max_fire_count: int = 15
+var fire_count: int = 0
+
 const FIRE = preload("res://scenes/enemies/fire.tscn")
 @onready var fire_container = $FireContainer
 @onready var area = $CollisionPolygon2D
@@ -9,10 +12,14 @@ func play_impale() -> void:
 	$AnimationPlayer.play("Impale")
 
 func ignite():
+	if fire_count >= max_fire_count:
+		return
 	var fire = FIRE.instantiate()
+	fire.fire_place = self
 	fire.position = get_random_pos(area.polygon)
 	fire.ignite()
 	fire_container.add_child(fire)
+	fire_count += 1
 
 func get_random_pos(points: PackedVector2Array) -> Vector2:
 	var rect = Rect2(points[0], Vector2.ZERO)
@@ -27,5 +34,9 @@ func get_random_pos(points: PackedVector2Array) -> Vector2:
 		
 		if Geometry2D.is_point_in_polygon(point, points):
 			return point
-
+	
 	return Vector2.ZERO
+
+func decrement() -> void:
+	fire_count -= 1
+	print("decrement")
